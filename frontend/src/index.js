3a8803306909d34f2ce504dd97e307e699dd9611
Blake Roses <rosesblake@yahoo.com>
2025-02-03 10:28:38 -0800

    still debugging refresh issue

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -11,7 +11,7 @@ import { ModalProvider } from "./contexts/ModalContext";
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
-    <BrowserRouter>
+    <BrowserRouter basename="/">
       <MessageProvider>
         <UserProvider>
           <ModalProvider>
